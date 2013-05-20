e31e633ecfb36effbc7c41a4968b7a2f91bd5b1d
Maxim Shvecov <maxim.shvecov@lge.com>
2013-05-20 10:41:28 +0400

    Small fix

--- src/fasttable/customfasttablewidget.h
+++ src/fasttable/customfasttablewidget.h
@@ -351,7 +351,7 @@ protected:
 
     void init(const bool aUseInternalData);
     void createLists();
-    virtual void deleteLists();        // TODO: FastTable: DO NOT FORGET IT
+    virtual void deleteLists();
 
     void keyPressEvent(QKeyEvent *event);
     bool focusNextPrevChild(bool next);
